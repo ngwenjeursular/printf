b42b75bb0943f7bca6689493fdd7746f7f88dcfd
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-20 15:08:43 -0700

    main.h

--- main.h
+++ main.h
@@ -10,4 +10,9 @@
 int _printf(const char *format, ...);
 int handler_specifier(const char *format, va_list total_args, char *bufr, int *index);
 void buffr(const char *buffer, int size);
+int printc(va_list list);
+int print_string(va_list list);
+int print_n(va_list list);
+int (*cmp_func2(const char a))(va_list);
+
 #endif
