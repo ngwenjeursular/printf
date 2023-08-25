7081f3aa8e9ccd3eb11e2562f8ecea026c6260e9
Philip Opiyo <opiyophilipo@gmail.com>
2023-08-25 03:57:04 -0700

    fixing error!

--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <limits.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <string.h>
@@ -26,9 +25,9 @@ typedef struct func_type
 
 
 /*utility*/
-int _strlen(const char *);
+/*int _strlen(const char *);
 int print(char *);
-char *itoa(long int, int);
+char *itoa(long int, int);*/
 
 /*printf.c*/
 int _printf(const char *format, ...);
@@ -40,16 +39,15 @@ int print_char(va_list args);
 int print_pct(va_list args);
 int print_dec(va_list args);
 int print_int(va_list args);
-int print_hexadecimal_upper(va_list list);
+/*int print_hexadecimal_upper(va_list list);
 int print_octal(va_list list);
 int print_hexadecimal_lower(va_list list);
-int print_unsigned(va_list list);
+int print_unsigned(va_list list);*/
 int print_binary(unsigned int num);
 
 /*handler.c */
-int handler(const char *, va_list);
-int percent_handler(const char *, va_list, int *);
-int print1char(char);
+/*int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *)*/
 int b_handler(va_list args);
 int p_handler(va_list args);
 void print_addr(void *ptr);
